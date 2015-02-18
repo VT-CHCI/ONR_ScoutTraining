844fbefc95aaafead6d17b816b7eb8708db0536f
FintanKelly <fintan.t.kelly@gmail.com>
2015-02-17 23:29:12 -0500

    Finished off the crate drop, pending further changes to it, and started working on functionality for Binocular view.

--- Assets/Scripts/custom/Binoculars.js
+++ Assets/Scripts/custom/Binoculars.js
@@ -1,9 +1,48 @@
 ﻿#pragma strict
 
-//var ftCanvas : 
+import System.Collections.Generic;
+
+var topSemi : Texture2D;
+var botSemi : Texture2D;
+var blackScreen : Texture2D;
+
+var cameras : Array;
+var currentTexture : GUITexture;
 
 function Start () {
-
+	topSemi = Resources.Load("Binocular-HalfT");
+	botSemi = Resources.Load("Binocular-HalfB");
+	blackScreen = Resources.Load("Black-Screen");
+	
+	cameras = Camera.allCameras;
+	
+	if(topSemi != null && botSemi != null && cameras.Count != 0) {
+		Debug.Log(true);
+	}
+	else {
+		Debug.Log(false);
+	}
+	
+	for(var cam in cameras) {
+		switch(cam.ToString()) {
+			case "Camera.FrontTop (UnityEngine.Camera)":
+				currentTexture = (cam as Camera).guiTexture;
+				currentTexture.texture = topSemi;
+				break;
+			case "Camera.FrontBottom (UnityEngine.Camera)":
+				currentTexture = (cam as Camera).guiTexture;
+				currentTexture.texture = botSemi;
+				break;
+			default:
+				currentTexture = (cam as Camera).guiTexture;
+				
+				if(currentTexture == null) {
+					break;
+				}
+				
+				currentTexture.texture = blackScreen;
+		}
+	}
 }
 
 function Update () {
